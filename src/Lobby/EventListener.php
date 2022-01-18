7553524caa1ed6eadbaae6c963f1c5a6f069b127
itoozh <74574377+itoozh@users.noreply.github.com>
2022-01-18 16:48:28 -0500

    Welcomer Message and spawn tp

--- src/Lobby/EventListener.php
+++ src/Lobby/EventListener.php
@@ -5,13 +5,22 @@ namespace Lobby;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
 
 class EventListener implements Listener {
 
     public function onJoin(PlayerJoinEvent $event) : void
     {
         $player = $event->getPlayer();
+        $player->getInventory()->setItem(4, ItemFactory::getInstance()->get(ItemIds::NAME_TAG)->setCustomName("§l§r§6Servers"));
         $event->setJoinMessage("§7[§2+§7] " . $player->getName());
+        $player->sendTitle("§l§6Welcome");
+        $player->sendSubTitle("§6Welcome " . $player->getName());
+        $player->setGamemode(GameMode::SURVIVAL());
+        $player->setHealth($player->getMaxHealth());
+        $player->getHungerManager()->setFood($player->getHungerManager()->getMaxFood());
+        $player->teleport(new Location(0, 100, 0,);
     }
     public function onQuit(PlayerQuitEvent $event) : void {
         $player = $event->getPlayer();
