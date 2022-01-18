2e7b91eb6e1392dbe092e85a4864cbe746036db1
itoozh <74574377+itoozh@users.noreply.github.com>
2022-01-18 17:37:59 -0500

    Update EventListener.php

--- src/Lobby/EventListener.php
+++ src/Lobby/EventListener.php
@@ -12,13 +12,14 @@ use pocketmine\player\GameMode;
 use pocketmine\player\Player;
 use pocketmine\entity\Location;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
 
 class EventListener implements Listener {
 
     public function onJoin(PlayerJoinEvent $event) : void
     {
         $player = $event->getPlayer();
-        $player->getInventory()->setItem(4, ItemFactory::getInstance()->get(ItemIds::NAME_TAG)->setCustomName("§l§r§6Servers"));
+        $player->getInventory()->setItem(4, ItemFactory::getInstance()->get(ItemIds::COMPASS)->setCustomName("§l§r§6Servers"));
         $event->setJoinMessage("§7[§2+§7] " . $player->getName());
         $player->sendTitle("§l§6Welcome");
         $player->sendSubTitle("§6Welcome " . $player->getName());
@@ -33,6 +34,18 @@ class EventListener implements Listener {
     }
 
     public function interact(PlayerInteractEvent $event) : void{
+        $player = $event->getPlayer();
+        $name = $event->getItem()->getCustomName();
+        if ($name) {
+            switch ($name) {
+                case "§l§r§6Servers":
+                    $player->sendMessage("Agregar Funcion que Abre UI");
+                    break;
+            }
+        }
+    }
+
+    public function rightclick(PlayerItemUseEvent $event) : void{
         $player = $event->getPlayer();
         $name = $event->getItem()->getCustomName();
         if ($name) {
