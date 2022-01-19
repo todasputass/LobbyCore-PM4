59675032a8abab345bfc8a96e927860077a0ff8b
Serg1hh <94806331+Serg1hh@users.noreply.github.com>
2022-01-19 15:12:31 -0300

    Update CreateForm.php

--- src/Lobby/Forms/CreateForm.php
+++ src/Lobby/Forms/CreateForm.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace Lobby\Forms;
+
+use jojoe77777\FormAPI\SimpleForm;
+use pocketmine\player\Player;
+use jojoe77777\FormAPI\entries\simple\Button;
+
+class CreateForm extends SimpleForm {
+
+    public function __construct(Player $inviter, Player $reciver, string $mode) {
+
+        if($inviter instanceof Player) {
+
+            $name=$inviter->getName();
+            parent::construc("Servers");
+            $this->addButton(new Button ("HCF"));
+        }
+    }
+}
+
