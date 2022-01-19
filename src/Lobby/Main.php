4cd8bd5d44257c097b54d218a89731357b393379
itoozh <74574377+itoozh@users.noreply.github.com>
2022-01-19 08:39:38 -0500

    asd

--- src/Lobby/Main.php
+++ src/Lobby/Main.php
@@ -8,25 +8,21 @@ use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\level\sound\GhastShootSound;
-use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 
-class Main extends PluginBase 
+class Main extends PluginBase implements Listener
 {
-
     public function onEnable() : void {
         $this->GetServer()->getPluginManager()->registerEvents(new EventListener(), $this);
         $this->GetLogger()->info("LobbyCore Enabled");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        @mkdir($this->getDataFolder());
         $this->saveResource("config.yml");
         $this->getConfig = new Config($this->getDataFolder() . "config.yml", Config::YAML);
     }
 
     public function onJoin(PlayerJoinEvent $event){
-        $player = $event->getPlayer();
-        $player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-        $player->getLevel()->sendSound(new GhastShootSound($player)); 
-        $player->sendTitle($this->getConfig()->get("Server-Name");
+        $player = $event->getPlayer(); 
+        $player->sendTitle($this->getConfig()->get("Server-Name"));
     }
 }
 
