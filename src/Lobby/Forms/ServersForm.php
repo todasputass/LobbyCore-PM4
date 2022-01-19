04809b639ac8e6f355b7e027f59789ec83bd0403
itoozh <74574377+itoozh@users.noreply.github.com>
2022-01-19 16:00:31 -0500

    Add Transfer in Form

--- src/Lobby/Forms/ServersForm.php
+++ src/Lobby/Forms/ServersForm.php
@@ -8,16 +8,20 @@ use cosmicpe\form\SimpleForm;
 use cosmicpe\form\types\Icon;
 use pocketmine\item\VanillaItems;
 use pocketmine\player\GameMode;
+use Lobby\Main;
 
 class ServersForm extends SimpleForm {
 
 	public function __construct() {
 		parent::__construct("Servers Selector", "Choose a option");
 		$this->addButton(new Button("HCF\nClick Me!"), function(Player $player, int $index) {
-			$player->setGamemode(GameMode::CREATIVE());
+			$data = Main::getInstance()->getConfig()->get('servers')['server1'];
+			$player->transfer($data['ip'], (int) $data['port']);
+
 		});
 		$this->addButton(new Button("Practice\nClick Me!"), function(Player $player, int $index) {
-			$player->setGamemode(GameMode::SURVIVAL());
+			$data = Main::getInstance()->getConfig()->get('servers')['server2'];
+			$player->transfer($data['ip'], (int) $data['port']);
 		});
 	}
 
