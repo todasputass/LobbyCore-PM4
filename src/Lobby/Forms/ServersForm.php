c14eb6089b4980046607276ffe0c3942b892d96a
000019 <96399692+JqkzDev@users.noreply.github.com>
2022-01-20 03:03:01 -0300

    Fixxx

--- src/Lobby/Forms/ServersForm.php
+++ src/Lobby/Forms/ServersForm.php
@@ -11,16 +11,16 @@ use pocketmine\player\GameMode;
 use Lobby\Main;
 
 class ServersForm extends SimpleForm {
+    
+    public function __construct() {
+        parent::__construct("Server Selector", "Choose a option");
+        $servers = Main::getInstance()->getConfig()->get('servers.available');
 
-	public function __construct() 
-{
-  parent::__construct("Server Selector", "Choose a option");
-  $servers = Main::getInstance()->getConfig()->get('servers.available');
-  foreach($servers as $server) { 
-    $this->addButton(new Button($server["server"] . "\nClick to join"), function(Player $player, int $index) use ($server): void{
-        $address = explode(":", $server["address"]); # el explode convierte la string en array, dividiendose por ":"
-        $player->transfer($address[0], $address[1], "Transfer to {$server["server"]}"); # Aqui sacamos la array 0 la cual contiene la IP ya que esta en el primer parametro
-    });
-  }
-}
+        foreach ($servers as $server) { 
+            $this->addButton(new Button($server["server"] . "\nClick to join"), function(Player $player, int $index) use ($server) : void {
+                $address = explode(":", $server["address"]); # el explode convierte la string en array, dividiendose por ":"
+                $player->transfer($address[0], (int) $address[1], "Transfer to {$server["server"]}"); # Aqui sacamos la array 0 la cual contiene la IP ya que esta en el primer parametro
+            });
+        }
+    }
 }
