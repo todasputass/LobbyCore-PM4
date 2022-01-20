21419067f804f8559237ce800063a822a67c2ff7
000019 <96399692+JqkzDev@users.noreply.github.com>
2022-01-20 02:54:59 -0300

    Update Main.php

--- src/Lobby/Main.php
+++ src/Lobby/Main.php
@@ -3,15 +3,7 @@
 namespace Lobby;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
-use pocketmine\Server;
-use pocketmine\Player;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\level\sound\GhastShootSound;
-use pocketmine\event\Listener;
 use pocketmine\utils\SingletonTrait;
-use pocketmine\event\player\PlayerExhaustEvent;
-use pocketmine\event\entity\EntityDamageEvent;
 use Lobby\session\SessionFactory;
 
 class Main extends PluginBase implements Listener {
@@ -25,11 +17,15 @@ class Main extends PluginBase implements Listener {
     }
 
     protected function onEnable() : void {
-        $this->sessionFactory = new SessionFactory;
-        $this->GetServer()->getPluginManager()->registerEvents(new EventListener(), $this);
-        $this->GetLogger()->info("LobbyCore Enabled");
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        # Setup config
         $this->saveResource("config.yml");
+        # Setup session factory
+        $this->sessionFactory = new SessionFactory;
+        # Register event handler
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+        
+        # Logger
+        $this->getLogger()->info("LobbyCore Enabled");
     }
     
     /**
