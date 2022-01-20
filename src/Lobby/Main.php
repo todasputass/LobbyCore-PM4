b08f0b49b04d291880846babaa8bfd05dc80c88d
itoozh <74574377+itoozh@users.noreply.github.com>
2022-01-20 00:34:48 -0500

    Migration

--- src/Lobby/Main.php
+++ src/Lobby/Main.php
@@ -31,21 +31,6 @@ class Main extends PluginBase implements Listener {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveResource("config.yml");
     }
-
-    public function onJoin(PlayerJoinEvent $event) {
-        $player = $event->getPlayer(); 
-        $player->sendTitle($this->getConfig()->get("Server-Name"));
-        $player->teleport($this->getServer()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
-    }
-    public function onExhaust(PlayerExhaustEvent $event): void {
-        $event->cancel(true);
-    }
-
-    public function onDamage(EntityDamageEvent $event) {
-        if($event->getCause() === EntityDamageEvent::CAUSE_FALL) {
-            $event->cancel(true);
-        }
-    }
     
     /**
      * @return SessionFactory
