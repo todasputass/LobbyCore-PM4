dc342b60faa2c7b06a7b517c85948eb807722c7c
itoozh <74574377+itoozh@users.noreply.github.com>
2022-01-20 12:49:11 -0500

    Fix Command bug, and add /hub, /lobby

--- src/Lobby/command/SpawnCommand.php
+++ src/Lobby/command/SpawnCommand.php
@@ -15,11 +15,11 @@ use pocketmine\utils\TextFormat;
 class SpawnCommand extends Command {
 
     public function __construct() {
-        parent::__construct('spawn', 'Use it to teleport to the world spawn');
+        parent::__construct('spawn', 'Use it to teleport to the world spawn', "", ["hub", "lobby"]);
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): void {
-        if($sender instanceof Player and isset($args[0])) {
+        if($sender instanceof Player) {
             SessionFactory::getSession($sender)->teleportToLobbyWorld();
         }
     }
