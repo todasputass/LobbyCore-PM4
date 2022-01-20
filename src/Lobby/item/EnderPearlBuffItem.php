09ebd58daad5b56c3cca5c5d45869d011b17196b
itoozh <74574377+itoozh@users.noreply.github.com>
2022-01-20 12:31:44 -0500

    EnderPearlBuff Nashe

--- src/Lobby/item/EnderPearlBuffItem.php
+++ src/Lobby/item/EnderPearlBuffItem.php
@@ -16,12 +16,13 @@ use pocketmine\player\Player;
 class EnderPearlBuffItem extends LobbyItem {
 
     public function __construct() {
-        parent::__construct(new ItemIdentifier(ItemIds::ENDER_PEARL, 0), "§l§6EnderPearl Buff");
+        parent::__construct(new ItemIdentifier(ItemIds::ENDER_PEARL, 0), "§r§7» §r§l§6EnderPearl Buff §r§7«");
     }
 
     public function onClickAir(Player $player, Vector3 $directionVector): ItemUseResult {
-        $player->setMotion($player->getDirectionVector()->multiply(0.3)->add(0, $player->getEyeHeight(), 0));
-        return ItemUseResult::SUCCESS();
+        $player->setMotion($player->getDirectionVector()->multiply(1.6));
+        $player->getInventory()->setItem(0, $this);
+        return ItemUseResult::FAIL();
     }
 
 }
