e329fecfa0bd6e520bb318297b80c7fc0020de4a
zOmArRD <omarsena25@gmail.com>
2022-01-29 22:05:26 +0100

    Finished Rainbow Armor Color

--- src/Lobby/listener/ItemListener.php
+++ src/Lobby/listener/ItemListener.php
@@ -8,14 +8,18 @@ namespace Lobby\listener;
 
 use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\event\Listener;
+use pocketmine\inventory\transaction\action\SlotChangeAction;
+use pocketmine\Server;
 
 class ItemListener implements Listener
 {
 
-    public function onTransaction(InventoryTransactionEvent $event): void
+    public function slot_change(InventoryTransactionEvent $event): void
     {
+        $player = $event->getTransaction()->getSource();
+
         foreach ($event->getTransaction()->getActions() as $action) {
-            if ($action->getSourceItem()->getNamedTag()->getTag("lobby") !== null) {
+            if (($action instanceof SlotChangeAction) && !Server::getInstance()->isOp($player->getName())) {
                 $event->cancel();
             }
         }
